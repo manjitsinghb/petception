6e5b7998c6c19d14d29b7a3eb4b9c555c712a1ab
manjit <Manjitsingh.bits@gmail.com>
2016-06-07 00:05:29 -0500

    Updated the index with angular and bootstrap

--- web/src/main/resources/public/angularjs/indexfs.js
+++ web/src/main/resources/public/angularjs/indexfs.js
@@ -3,8 +3,8 @@ var indexFs = angular.module('index',[]);
 
 indexFs.controller('PetInfoController',function($scope,$http){
 $http.get('http://localhost:8080/getAllPets').
-        success(function(data) {
-            $scope.petInfo = data;
+        then(function(response) {
+            $scope.petInfo = response.data;
         });
 });
 })();
