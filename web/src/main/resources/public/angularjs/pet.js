525104f26063366a72c10f731b4c6843a76686c6
manjit <Manjitsingh.bits@gmail.com>
2016-06-09 22:26:42 -0500

    Updated the index with angular and bootstrap

--- web/src/main/resources/public/angularjs/pet.js
+++ web/src/main/resources/public/angularjs/pet.js
@@ -4,10 +4,22 @@ var petModule = angular.module('petModule',[]);
 petModule.directive('petInfo',function(){
 return{
     restrict:'E',templateUrl:'/petAddInfo.html',controller:['$http',function($http){
+        this.success=0;
+        this.error=0;
         this.addPet=function(mypet)
         {
+        this.success=0;
+        this.error=0;
         petInfo={'pet':mypet};
-        $http.post('http://localhost:8080/addPet',petInfo,{headers:{'Content-Type':'application/json'}});
+        $http.post('http://localhost:8080/addPet',petInfo,{headers:{'Content-Type':'application/json'}})
+        .then(function(data){
+        console.log("success");
+            this.success=-1;
+        },function(data){
+        console.log("error");
+            this.error=-1;
+        })
+        ;
         //submit this petInfo to service
         }
     }],controllerAs:'petAddInfo'
