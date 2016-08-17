eeb7961f68409e5ba275c9d89de6f531b5889c82
manjit <Manjitsingh.bits@gmail.com>
2016-08-16 22:49:43 -0500

    Docker Config

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-sh java -jar /home/app/web-1.0-SNAPSHOT/web-1.0-SNAPSHOT.jar
-sh java -jar /home/app/rest-1.0-SNAPSHOT/rest-1.0-SNAPSHOT.jar
-sh java -jar /home/app/oauth-1.0-SNAPSHOT/rest-1.0-SNAPSHOT.jar
+java -jar /home/app/web-1.0-SNAPSHOT/web-1.0-SNAPSHOT.jar
+java -jar /home/app/rest-1.0-SNAPSHOT/rest-1.0-SNAPSHOT.jar
+java -jar /home/app/oauth-1.0-SNAPSHOT/rest-1.0-SNAPSHOT.jar
