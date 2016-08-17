500d0aeb3718362988988cf39c13d8da22b25c0c
manjit <Manjitsingh.bits@gmail.com>
2016-08-17 00:29:25 -0500

    Docker Config

--- dao/src/main/java/com/petception/beans/DaoConfiguration.java
+++ dao/src/main/java/com/petception/beans/DaoConfiguration.java
@@ -38,7 +38,7 @@ public class DaoConfiguration {
     @Bean
     public MongoClient getMongoClient()
     {
-        return new MongoClient();
+        return new MongoClient("mongo",27017);
     }
 
     @Bean
